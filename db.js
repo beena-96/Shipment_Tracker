f89aee2e58dcf8f08c61d18832d5b3f3224303c4
beena-96 <114981809+beena-96@users.noreply.github.com>
2023-09-29 15:26:12 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost', // Change this to your PostgreSQL server's host
-  database: 'shipment_trackerDb',
-  password: 'PostgreSQL',
-  port: 5432, // Default PostgreSQL port
+  user: '',
+  host: '', // Change this to your PostgreSQL server's host
+  database: '',
+  password: '',
+  port: , // Default PostgreSQL port
 });
 
 module.exports = pool;
